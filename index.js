00ba45b8d3b15c0aed783d08d6799f35cec5f164
Paulo Fernando Pezzetti de Souza <67803223+paulopezzetti@users.noreply.github.com>
2021-09-16 20:52:01 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const PORT = 3000;
 const HOST = '0.0.0.0';
 const app = express ();
 app.get ('/', (req, res) => {
-    res.send('Build Automatizado via jenkins+webhook2')
+    res.send('TESTE TECNICO TRADE')
 
 });
 
