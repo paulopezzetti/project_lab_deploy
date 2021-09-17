ab4a3e9ff100a3f4b7f3ba314ede1db0bba1ab92
Paulo Fernando Pezzetti de Souza <67803223+paulopezzetti@users.noreply.github.com>
2021-09-16 21:32:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const PORT = 3000;
 const HOST = '0.0.0.0';
 const app = express ();
 app.get ('/', (req, res) => {
-    res.send('TESTE TECNICO TRADE')
+    res.send('ASsdasdasdasdasdasdE')
 
 });
 
